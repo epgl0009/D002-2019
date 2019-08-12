f982b68e47fcf35dea4ac64a342cce1e98fbeb82
epgl0009 <53845766+epgl0009@users.noreply.github.com>
2019-08-12 12:40:53 +0800

    Add files via upload

---
+++ L3/Q3.py
@@ -0,0 +1,6 @@
+list=[]
+for l in range(0,10):
+    word=input("Enter a word: ")
+    if(word[0]=="A" or word[0]=="E" or word[0]=="I" or word[0]=="O" or word[0]=="U"): 
+        list.append(word)
+print(list)
